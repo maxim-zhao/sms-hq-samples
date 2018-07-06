4e4e1a7d19e5b891c67141dcb4883849dbd638ef
Maxim <maxim@smspower.org>
2018-07-06 19:19:32 +0100

    Louder samples fro Altered Beast

--- Altered Beast/assets.cmd
+++ Altered Beast/assets.cmd
@@ -1,3 +1,3 @@
-path %path%;c:\Program Files\sox
-for %%f in ("original samples\*.wav") do sox "%%f" "%%~nxf" channels 1 silence 1 0.1 1%% reverse silence 1 0.1 1%% reverse rate 8000
-for %%f in (*.wav) do ..\..\..\c\pcmenc\encoder\x64\Release\pcmenc.exe -rto 1 -p 4 -dt1 12 -dt2 12 -dt3 423 -a 100 "%%f"
+rem path %path%;c:\Program Files\sox
+rem for %%f in ("original samples\*.wav") do sox "%%f" "%%~nxf" channels 1 silence 1 0.1 0.1%% reverse silence 1 0.1 0.1%% reverse rate 8000 
+for %%f in (*.wav) do ..\..\..\c\pcmenc\encoder\x64\Release\pcmenc.exe -rto 1 -p 4 -dt1 12 -dt2 12 -dt3 423 -a 115 "%%f"
