f8a8ddc2823a6c5d3e317b9aaafe41cca8f679d1
Maxim <maxim@smspower.org>
2025-12-24 18:15:42 +0000

    Fixed readme and improved patch to no longer need slot 1 paging

--- Out Run/outrun-arcade-voices.sms.asm
+++ Out Run/outrun-arcade-voices.sms.asm
@@ -20,11 +20,22 @@ banks 20
 .background "outrun.sms"
 ; We don't need to clear all this space - but it's all no longer used
 ;.unbackground $0e61 $0e71 ; Mark III splash phase 2
-;.unbackground $0e78 $0eec ; Code for showing Mark III splash
-;.unbackground $0f15 $0f3a ; Mark III tilemap
+.unbackground $0e78 $0eec ; Code for showing Mark III splash
+.unbackground $0f15 $0f3a ; Mark III tilemap
 .unbackground $0f3b $1022 ; Mark III tiles
-.unbackground $7fd5 $7fef ; Space before Sega header, at least room for the SDSC tag...
+.unbackground $7fd5 $7fef ; Space before Sega header, needed for the SDSC tag...
 .unbackground $7ff0 $7fff ; Sega header
+
+; Functions in the original that are useful to us
+.define EndOfMarkIIILogo $0e72
+.define MuteAudio $5c93
+
+; Memory locations
+.define HasFM $c002
+.define HavePlayedCongratulations $dc00 ; This seems to be an unused memory location
+
+; SMS ports
+.define Port_AudioControl $f2
 
 ; We add an SDSC header, which rewrites the header so we also restore some values to match the original ROM
 .smsheader
@@ -37,11 +48,11 @@ banks 20
 ; Patch for Mark III logo showing
 .org $0010 ; Lookup table for game state
 .section "Mark III logo patch" overwrite
-  .dw $0e72 ; patch to where it goes if in export mode
+  .dw EndOfMarkIIILogo ; patch to where it goes if in export mode
 .ends
 
 
-; Patches to hook to play samples
+; Patches to hook to play samples. These all overwrite three bytes in the code, which we need to replace later.
 .bank 0 slot 0
 .orga $16b3 ; Start line, playing the sound effect before music
 .section "Start line patch" overwrite
@@ -55,97 +66,100 @@ banks 20
 .ends
 
 .bank 0 slot 0
-.orga $1e1a
+.orga $1e1a ; Frame handler for ending animations. We want to hook the first frame of the animation.
 .section "Ending patch" overwrite
   call SampleTrampolineCongratulations
 .ends
 
 .bank 0 slot 0
-.section "Trampolines" free
-_PlaySample:
-  ; Page in the sample player
-  ld a, ($ffff)
-  push af
-    ; Now play the sample
-    ld a, :PlaySample
-    ld ($ffff), a
-    call PlaySample
-  pop af
-  ld ($ffff), a
-  ret
-
+.section "Trampoline 1" free
 SampleTrampolineGetReady:
   ; What we replaced to get here
   ld ($c000), a
+
   push hl
   push bc
     ld hl, GetReady 
     ld c, :GetReady
-    call _PlaySample
+    call PlaySample
+
     ; Wait for VBlank before continuing to avoid a timing issue
 -:  in a, ($7e)
-    cp 192
+    sub 192
     jr nz, -
+
+    ; Later on we want to play Congratulations only once, using a flag.
+    ; This is a good place to clear the flag as a is zero.
+    ld (HavePlayedCongratulations), a
   pop bc
   pop hl
   ret
+.ends
 
+.section "Trampoline 2" free
 SampleTrampolineCheckpoint:
   ; What we replaced to get here
   ld hl, $c000
+
   push hl
   push bc
     ld hl, Checkpoint
     ld c, :Checkpoint
     ; Disable interrupts else the sample is chopped up
+PlaySampleWithInterruptsOff:
+    ; Other samples can reuse these bytes...
     di
-      call _PlaySample
+      call PlaySample
     ei
   pop bc
   pop hl
   ret
+.ends
 
+.section "Trampoline 3" free
 SampleTrampolineCongratulations:
   ; This is called repeatedly, we want to only hook the first call
   push af
-    ld a, ($dc00) ; This seems to be an unused memory location, zero-initialised
+    ld a, (HavePlayedCongratulations)
     or a
-    call z, +
+    call z, PlayCongratulations
   pop af
   ; What we replaced to get here
   jp $25a2
-  
-+:push bc
+.ends
+
+.section "Play congratulations" free
+PlayCongratulations:
+  ; Set the flag
+  ld a, 1
+  ld (HavePlayedCongratulations), a
+  push hl
+  push bc
     ld hl, Congratulations
     ld c, :Congratulations
-    di
-    call _PlaySample
-    ei
-  pop bc
-  ld a, 1
-  ld ($dc00), a
-  ret
-    
+    jp PlaySampleWithInterruptsOff
 .ends
 
 ; The new sample player code
-
-.section "Replayer" superfree
+.section "Sample multi-bank player" free
 .include "../Common/replayer_core_p4_rto3_8kHz.asm"
 .ends
-.section "Sample multi-bank player" superfree
+
+.section "PSG settings for sample" free
 PSGSampleSettings:
 .db $9f $bf $df $ff ; Maximum attenuation on all channels
 .db $80 $00 ; Frequency 0 on tone channels
 .db $a0 $00 
 .db $c0 $00
+.ends
 
+.section "Sample player" free
 PlaySample:
-  ; Pause music
-  call $5c93
+  ; Mute music/SFX
+  call MuteAudio
   ; Disable FM (regardless of whether it was enabled)
   xor a
-  out ($f2),a
+  out (Port_AudioControl),a
   ; Set PSG channel settings
   push hl
   push bc
@@ -154,26 +168,26 @@ PlaySample:
     otir
   pop bc
   pop hl
-  ; Page sample into slot 1
-  ld a,($fffe)
+  ; Page sample into slot 2
+  ld a,($ffff)
   push af
     ld a,c
-    ld ($fffe),a
+    ld ($ffff),a
     ld b,(hl) ; block count
     inc hl
 -:  push bc
       call PLAY_SAMPLE
     pop bc
-    ld hl,$fffe
+    ld hl,$ffff
     inc (hl)
-    ld hl,$4000
+    ld hl,$8000
     djnz -
   pop af
   ; Restore paging
-  ld ($fffe),a
+  ld ($ffff),a
   ; Restore FM mode
-  ld a,($c002) ; 1 for FM, 0 for PSG
-  out ($f2),a 
+  ld a,(HasFM) ; 1 for FM, 0 for PSG
+  out (Port_AudioControl),a 
   ret
 .ends
 
@@ -181,7 +195,7 @@ PlaySample:
 .include "../Common/addfile.asm"
 .define databank 16
 .define bankspace $4000
-.bank databank slot 1
+.bank databank slot 2
 .org 0
 GetReady:        addfile "getready.wav.pcmenc"
 Checkpoint:      addfile "checkpoint.wav.pcmenc"
--- Out Run/readme.txt
+++ Out Run/readme.txt
@@ -1,8 +1,9 @@
 Out Run Arcade Voices patch
 ===========================
 
-This patch replaces the voice samples in Out Run for Sega Master System with data taken from the original arcade game, played using high-quality pcmenc data.
+This patch adds voice samples to Out Run for the Sega Master System, using data taken from the original arcade game, played using high-quality pcmenc data.
 
 Source is available at https://github.com/maxim-zhao/sms-hq-samples
 
 v1.0 - initial release
+v1.1 - removed use of paging slot 1
